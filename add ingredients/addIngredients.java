d692cefd2bcbcf3ef7c6a5674c12bd80fae9c2b2
Shammamah Hossain <shammamah.hossain@gmail.com>
2016-09-17 17:25:36 -0400

    Update button code for addIngredients

--- add ingredients/addIngredients.java
+++ add ingredients/addIngredients.java
@@ -7,5 +7,46 @@ public class addIngredients extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_ingredients);
+
+        Button goDoneAdding= (Button) findViewById(R.id.button_doneAddingIngredient);
+        // this button adds the ingredient and goes back to list view of ingredients
+        // button ID for "done" button is "doneAddingIngredient"
+        goDoneAdding.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            /* Function: performs everything that happens when "done" button clicked */ 
+            public void onClick(View view){
+            	// TODO: get name from text field
+
+            	Ingredient newIngredient = new Ingredient(name);
+            	ingredients.add(Ingredient newIngredient);
+            	// create a new ingredient and add it to 
+            	// ArrayList<Ingredients> ingredients
+            	
+                Intent intent= new Intent(addIngredients.this, viewIngredients.class);
+                // it goes from addIngredients to viewIngredients
+                startActivity(intent);
+            }
+        });
+
+        Button goAddMore= (Button) findViewById(R.id.button_addMore);
+        // this button adds the ingredient and goes back to the add more screen
+        // button ID for "add more" button is "addMore"
+        goDoneAdding.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            /* Function: performs everything that happens when "add more" button clicked */ 
+            public void onClick(View view){
+
+                Ingredient newIngredient = new Ingredient(name);
+            	ingredients.add(Ingredient newIngredient);
+            	// create a new ingredient and add it to 
+            	// ArrayList<Ingredients> ingredients
+            	
+                Intent intent= new Intent(addIngredients.this, addIngredients.class);
+                // it goes from addIngredients to addIngredients
+                startActivity(intent);
+            }
+        });
     }
 }
