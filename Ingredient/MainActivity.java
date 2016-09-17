1d6877ae344c8d35931f4689437f719cb7bfe0e4
Shammamah Hossain <shammamah.hossain@gmail.com>
2016-09-17 12:25:36 -0400

    Add ingredient array constructor, array index variable; begin ListView adapter and add/remove functions

---
+++ Ingredient/MainActivity.java
@@ -0,0 +1,50 @@
+/* SAMPLE CODE */ 
+/*public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        ListView listView1 = (ListView) findViewById(R.id.listView1);
+        
+        String[] items = { "Milk", "Butter", "Yogurt", "Toothpaste", "Ice Cream" };
+        
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, items);
+        
+        listView1.setAdapter(adapter);
+    }
+}*/
+
+Ingredient[] ingredients = new Ingredient[50];
+// initialize ingredient array 
+
+int ingredientIndex = 0; 
+// keep track of where we are in the array 
+
+ 
+/* Function: add ingredient to array 
+	Input: string for name 
+	Return val: ingredient that was just created */   
+public Ingredient addIngredient(String name){
+
+	if(ingredientIndex>=ingredients.length){
+
+	}
+
+    Ingredient newIngredient = new Ingredient(name);
+    ingredient[ingredientIndex] = newIngredient; 
+    ingredientIndex++; 
+
+    return newIngredient; 
+}
+
+ListView ingredientListView = (Listview) findViewById(R.id.ingredientListView); 
+// initialize ingredient list view 
+
+ArrayAdapter<Ingredient> adapter = new ArrayAdapter<Ingredient>(this, android.R.layout.simple_list_item_1, ingredients);
+// initialize adapter 
+
+ingredientListView.setAdapter(adapter); 
+// this will convert ingredients array to list view 
