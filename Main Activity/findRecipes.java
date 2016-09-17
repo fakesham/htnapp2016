266e29202590ed27904deb7e42465bd4fca21095
jiayinchen <jiayin.chen15@gmail.com>
2016-09-17 15:52:46 -0400

    added the main activities of the app (welcomepage, etc...)

---
+++ Main Activity/findRecipes.java
@@ -0,0 +1,13 @@
+package com.example.jiayin.foodhtn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class findRecipes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_recipes);
+    }
+}
