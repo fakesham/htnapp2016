09b7dda900c30ead6de99f309ec6e30763fa80d7
leonore13 <shinjitsu13@gmail.com>
2016-09-17 23:38:05 -0400

    Add everything back

---
+++ app/src/main/java/com/example/mammam/htn2016/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.mammam.htn2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* SHAM'S RESPONSIBILITY */
+        ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
+        // initialize ingredient arraylist
+        /* END SHAM'S RESPONSIBILITY */
+
+        Button goViewIngredients= (Button) findViewById(R.id.button_viewIngredients);
+        goViewIngredients.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent= new Intent(MainActivity.this, viewIngredients.class );
+                startActivity(intent);
+
+            }
+
+        });
+
+        Button goFindRecipes= (Button) findViewById(R.id.button_findRecipes);
+        goFindRecipes.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent= new Intent(MainActivity.this, findRecipes.class );
+                startActivity(intent);
+
+            }
+
+        });
+
+
+
+    }
+}
+
---
+++ app/src/main/java/com/example/mammam/htn2016/viewIngredients.java
@@ -0,0 +1,16 @@
+package com.example.mammam.htn2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class viewIngredients extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_ingredients);
+    }
+
+
+}
